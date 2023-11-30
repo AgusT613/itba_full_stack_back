3c4c22e7ccf9beca0a9e58ca4978102eca6fc262
AgusT613 <torresagustin613@gmail.com>
2023-11-30 14:27:32 -0300

    Nuevo modelo y modificacion de vista: Prestamo por sucursal

--- app/prestamos/admin.py
+++ app/prestamos/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Prestamo
+from .models import Prestamo, Sucursal
 
-admin.site.register(Prestamo)
+admin.site.register([Prestamo, Sucursal])
--- app/prestamos/models.py
+++ app/prestamos/models.py
@@ -2,8 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Sucursal(models.Model):
+    nombre = models.CharField(max_length=150)
+    direccion = models.CharField(max_length=250)
+
+    def __str__(self) -> str:
+        return f"{self.nombre}, {self.direccion}"
+
+
 class Prestamo(models.Model):
     cliente = models.ForeignKey(User, on_delete=models.CASCADE)
+    sucursal = models.ForeignKey(Sucursal, on_delete=models.DO_NOTHING, null=True)
     tipo_prestamo = models.CharField(
         choices=[
             ("hipotecario", "Prestamo hipotecario"),
